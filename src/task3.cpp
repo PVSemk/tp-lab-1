ec26bc3e7d3999585b80b19e2fb2e196c0be52f1
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2018-10-20 14:05:34 +0300

    Update task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -2,15 +2,14 @@
 #include <task3.h>
 
 unsigned long long sumPrime(unsigned int hbound) {
-	int*a = new int[hbound + 1];
+	int*a = new int[hbound + 1]{0};
 	unsigned long long int sum = 0;
-	int i = 2, j;
+	unsigned int i = 2, j;
 	while (i <= hbound) {
-		if (a[i] != 0) {
+		if (a[i] == 0) {
 			sum += i;
-			j = i;
-			for (j; j < (hbound + 1); j += i)
-				a[j] = 0;
+			for (j=i; j < (hbound + 1); j += i)
+				a[j] = 1;
 		}
 		i++;
 	}
