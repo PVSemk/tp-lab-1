8f8217067fe586de355fda27593a5c78f53a7581
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2018-10-20 14:11:34 +0300

    Update task5.cpp

--- src/task5.cpp
+++ src/task5.cpp
@@ -8,8 +8,11 @@ void split(char ***result, int *N, char *buf, char ch) {
 			*N += 1;
 		}
 	}
+	if (*N!=0) 
+		*N += 1;
+	else return;
 	char *tmp = new char[i + 1];
-	*result = new char *[*N + 1];
+	*result = new char *[*N];
 	(*result)[0] = tmp;
 	for (i = 0; buf[i]; i++) {
 		tmp[i] = buf[i];
