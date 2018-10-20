4ea155e829e8010f6c009f2a162984396f7dde61
Vadik Alperovich <vadikalperovich@mail.ru>
2018-10-20 21:17:21 +0400

    b1

--- src/task5.cpp
+++ src/task5.cpp
@@ -5,30 +5,31 @@ using namespace std;
 void split( char ***result, int *N,const  char *buf, char ch)
 {
 	int sepCount = 0;
-	
+	int j;
 	for(int i=0;buf[i]!='\0';i++)
 	{
 		if (buf[i] == ch) {
 			sepCount++;
 		}
+		j = i;
 	}
 	(*result) = (char **)malloc((sepCount + 1) * sizeof(char *));
 
-	char *part = new char[strlen(buf) + 1];
+	char *part = new char[j + 1];
    
 	sepCount = 1;
-	int i = 0;
+	j = 0;
 	(*result)[0] = part;
-	while (buf[i] != '\0') {
-		part[i] = buf[i];
-		if (buf[i] == ch) {
-			(*result)[sepCount] = part+i+1;
-			part[i] = '\0';
+	while (buf[j] != '\0') {
+		part[j] = buf[j];
+		if (buf[j] == ch) {
+			(*result)[sepCount] = part+(j+1);
+			part[j] = '\0';
 			sepCount++;
 		}
-		i++;
+		j++;
 	}
-	part[i] = '\0';
+	part[j] = '\0';
 	*N = sepCount;
 
 }
