7fc0648824eada21a9496e3aeb65ca8e21cd375d
Vadik Alperovich <vadikalperovich@mail.ru>
2018-10-20 22:55:00 +0400

    b1.1

--- src/task1.cpp
+++ src/task1.cpp
@@ -3,21 +3,13 @@
 #include <iostream>
 using namespace std;
 
-unsigned long findValue(unsigned int min, unsigned max)
-{
-	long int value = 1;
-	bool flag = 0;
-	for (int i = min; i <= max; i++)
-	{
-		for (int j = i; j > 0; j--)
-		{
-			if (value % j == 0 && i % j == 0)
-			{
-				value *= (i / j);
-				flag = 1;
-				break;
-			}
-		}
+unsigned int __gcd(unsigned int a, unsigned int b) {
+	return (b ? __gcd(b, a % b) : a);
+}
+unsigned long findValue(unsigned int min, unsigned max) {
+	unsigned long lcm = 1;
+	for (auto i = min; i <= max; i++) {
+		lcm = (lcm * i) / __gcd(lcm, i);
 	}
-	return value;
+	return lcm;
 }
