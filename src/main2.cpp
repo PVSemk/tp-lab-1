47338bbc80e74dec999dd3db22f0529015d7e44c
Vadik Alperovich <vadikalperovich@mail.ru>
2018-10-21 18:02:56 +0400

    b1

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,13 +1,10 @@
-#include <iostream>
-
-
-using namespace std;
+#include <task2.h>
 
 int main()
 {
-	/*int checkit = 1031;
+	int checkit = 1031;
 	cout <<checkit<<"  "<< checkPrime(checkit) << endl;
 	cout <<checkit<<"th  simple number is "<< nPrime(checkit) << endl;
-	cout <<"next simple number after "<<checkit<<" is "<<nextPrime(checkit) << endl;*/
+	cout <<"next simple number after "<<checkit<<" is "<<nextPrime(checkit) << endl;
 	return 0;
 }
--- src/main3.cpp
+++ src/main3.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
-//#include "task3.h"
-using namespace std;
+
+#include "task3.h"
+
 
 int main()
 {
-	/*int hbound = 2000000;
-	cout << "sum of all simple numbers up to " <<hbound<<" is "<< sumPrime(hbound) << endl;*/
+	int hbound = 2000000;
+	cout << "sum of all simple numbers up to " <<hbound<<" is "<< sumPrime(hbound) << endl;
 	return 0;
 }
 
