3bfe7e5524b509d26d283a9bf6bc3b3e21082294
Maksimov Anton <john-konnor@rambler.ru>
2018-10-23 13:16:27 +0400

    18 attempt

--- src/task2.cpp
+++ src/task2.cpp
@@ -16,20 +16,48 @@ bool checkPrime(unsigned int value)
 	}
 	return true;
 }
-unsigned long long nPrime(unsigned n) {
-		unsigned j = n;
-		unsigned long long i = 2;
-		while (j) 
-		{
-			if (checkPrime(i) == true) 
-			{
-				j--;
-			}
-			i++;
+unsigned long long nPrime(unsigned n)
+{
+	int size = n; 
+	int *primes = new int[n]; 
+	int *numbers = new int[size]; 
+
+	for (int i = 0; i < size; i++)
+		numbers[i] = i; 
+
+	primes[0] = 2; 
+	int i = 0; 
+
+	while (i < n) {
+		int p = primes[i++]; 
+
+		for (int j = p * 2; j < size; j += p)
+			numbers[j] = 0; 
+
+		while (numbers[p + 1] == 0)
+			p++; 
+
+		if (p + 1 >= size) { 
+			int *tmp = new int[size * 2];
+
+			for (int k = 0; k < size; k++)
+				tmp[k] = numbers[k];
+
+			delete[] numbers;
+
+			size *= 2;
+			numbers = tmp;
+
+			for (int j = size / 2; j < size; j++)
+				numbers[j] = j; 
+
+			i = 0; 
 		}
-		i--;
-		return i;
+		else
+			primes[i] = p + 1; 
 	}
+	return primes[n - 1];
+}
 unsigned long long nextPrime(unsigned long long value)
 {
 	value++;
