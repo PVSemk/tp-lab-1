37dbd9661fb35e48060ad13e6b836bb732ab616d
kislN <43805765+kislN@users.noreply.github.com>
2018-10-23 16:55:11 +0300

    Update task3.cpp
    task3.cpp

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,10 +1,24 @@
 #include <task3.h>
-unsigned long long sumPrime(unsigned int hbound) {
-	unsigned long long sum = 0, k = 0;
-	for (unsigned int i = 2; i < hbound; i++) {
-		if (checkPrime(i)) {
-			sum = sum + i;
+unsigned long long sumPrime(unsigned int hbound)
+{
+	int *z = new int[hbound + 1]{};
+
+	for (int i = 2; i*i<=hbound; i++)
+		if (z[i] == 0)
+		{
+			for (int j = i * i; j <= hbound; j = j + i)
+			{
+				z[j] = 1;
+			}
 		}
+
+	long long int sum = 0;
+
+	for (int i = 2; i < hbound; i++)
+	{
+		if (z[i] == 0)
+			sum = sum + i;
 	}
+
 	return sum;
 }
