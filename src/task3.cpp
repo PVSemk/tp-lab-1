be44e61dfe15b560c8c1794491af31fc76f494dc
rvmurzaev <romikbest5@gmail.com>
2018-10-23 15:16:29 +0300

    void tasks.4

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <task3.h>
 #include <task2.h>
+#include <cmath>
 
 unsigned long long sumPrime(unsigned int hbound)
 {
 	unsigned long long summ=0;
-	for (unsigned int i = 2; i < hbound; i++) 
+	for (unsigned int i = 2; i < sqrt(hbound); i++) 
 	{
 		if (checkPrime(i))
 			summ = summ + i;
