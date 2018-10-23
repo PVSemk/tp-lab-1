0193b0d9449fa83f9d5699e5ad0aa0aa64395708
rvmurzaev <romikbest5@gmail.com>
2018-10-23 16:15:08 +0300

    void tasks.9

--- src/main4.cpp
+++ src/main4.cpp
@@ -3,6 +3,12 @@
 #include <algorithm>
 #include <task4.h>
 
-int main() {
-return 0;
+int main()
+{
+	int c ;
+	char a[] = "564564536", b[] = "1532452321";
+	char *x = a, *y = b;
+	std::cout << sum(x, y) << std::endl;
+	std::cin >> c;
+    return 0;
 }
--- src/task2.cpp
+++ src/task2.cpp
@@ -2,26 +2,38 @@
 #include <task2.h>
 #include <cmath>
 
-bool checkPrime(unsigned int value)  //- проверка числа на простоту.
-{
-	for (int i = 2; i<=sqrt(value); i = i++)
-	{
-		if (value%i == 0)
+bool checkPrime(unsigned int value) {
+	for (long long i = 2; i <= sqrt(value); i++) {
+		if (value % i == 0)
 			return false;
 	}
 	return true;
 }
-unsigned long long nextPrime(unsigned long long value) //- нахождение ближайшего следующего простого числа к value.
-{
-	while (1)
-	{
-		value++;
-		if (checkPrime(value))
-			return value;
+
+unsigned long long nextPrime(unsigned long long value) {
+	value++;
+	if (value % 2 == 0) {
+		if (checkPrime(value) == 1) return value;
+		else value++;
+	}
+	while (value % 3 != 0) {
+		if (checkPrime(value) == 1) return value;
+		value += 2;
+	}
+	if (checkPrime(value) == 1) return value;
+	value += 2;
+	int n = 1;
+	while (1) {
+		if (n != 3) {
+			if (checkPrime(value) == 1) return value;
+			n++;
+		}
+		else n = 1;
+		value += 2;
 	}
 }
-unsigned long long nPrime(unsigned n) //- нахождение n - ого простого числа(в ряду).
-{
+
+unsigned long long nPrime(unsigned n) {
 	unsigned long long int np = 2;
 	for (int i = 1; i < n; i++) {
 		np = nextPrime(np);
