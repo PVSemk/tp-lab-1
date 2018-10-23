f8bd4a22c02ee07674f256cc58057bb433cec273
Maksimov Anton <john-konnor@rambler.ru>
2018-10-23 12:01:19 +0400

    14 attempt

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,21 +1,5 @@
 #include <iostream>
 #include <task3.h>
-bool checkPrime(unsigned int value)
-{
-	if ((value == 2) or (value == 3))
-	{
-		return true;
-	}
-	if ((value == 4) or (value == 6) or (value == 8) or (value == 10)) { return false; }
-	for (int i = 3; i <= value; i += 2)
-	{
-		if ((value % i == 0) or (value % 2 == 0))
-		{
-			return false;
-		}
-	}
-	return true;
-}
 unsigned long long sumPrime(unsigned int hbound)
 {
 	long long sum = 0;
