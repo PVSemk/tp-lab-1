f91da1708f2d6e5b8a0fa0fcf5f656d886d97193
rvmurzaev <romikbest5@gmail.com>
2018-10-23 16:08:36 +0300

    void tasks.8

--- src/task2.cpp
+++ src/task2.cpp
@@ -2,30 +2,16 @@
 #include <task2.h>
 #include <cmath>
 
-bool checkPrime(unsigned int value)
+bool checkPrime(unsigned int value)  //- проверка числа на простоту.
 {
-	for (int i = 2; i<=sqrt(value); i = i ++)
+	for (int i = 2; i<=sqrt(value); i = i++)
 	{
 		if (value%i == 0)
 			return false;
 	}
 	return true;
 }
-unsigned long long nPrime(unsigned n)
-{
-	unsigned long long int value = 1;
-	int counter = 0;
-	while (counter != n)
-	{
-		value++;
-		if (checkPrime(value))
-		{
-			counter++;
-		}
-	}
-	return value;
-}
-unsigned long long nextPrime(unsigned long long value)
+unsigned long long nextPrime(unsigned long long value) //- нахождение ближайшего следующего простого числа к value.
 {
 	while (1)
 	{
@@ -33,6 +19,14 @@ unsigned long long nextPrime(unsigned long long value)
 		if (checkPrime(value))
 			return value;
 	}
+}
+unsigned long long nPrime(unsigned n) //- нахождение n - ого простого числа(в ряду).
+{
+	unsigned long long int np = 2;
+	for (int i = 1; i < n; i++) {
+		np = nextPrime(np);
+	}
+	return np;
 }
 
 
