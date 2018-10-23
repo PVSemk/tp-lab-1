6486af35e0e32eefa739ecd1226bf6796f2a4923
rvmurzaev <romikbest5@gmail.com>
2018-10-23 14:33:47 +0300

    task3

---
+++ src/main3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <task3.h>
+#include "task2.cpp"
+
+int main()
+{
+	int n;
+	std::cout << sumPrime(10);
+	std::cin >> n;
+    return 0;
+}
---
+++ src/task3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <task3.h>
+#include "task2.cpp"
+#include <task2.h>
+
+unsigned long long sumPrime(unsigned int hbound)
+{
+	unsigned long long summ=0;
+	for (unsigned int i = 2; i < hbound; i++) 
+	{
+		if (checkPrime(i))
+			summ = summ + i;
+	}
+	return summ;
+}
+
