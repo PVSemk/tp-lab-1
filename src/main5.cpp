8ab9fb56902952a69aad835c8b275a7ddf3a3390
Chernozemova <44014905+Chernozemova@users.noreply.github.com>
2018-10-23 14:51:54 +0300

    Update main5.cpp

--- src/main5.cpp
+++ src/main5.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int main()
 {
-	char buf[] = "123 32 47549 1";
+	char buf[] = "123.32.47549.1";
 	int N = 0;
+	char ch = '.';
 	char **result = nullptr;
 
-	split(&result, &N, buf, ' ');
+	split(&result, &N, buf, ch);
 
 	cout << "0 - " << result[0] << " 1 - " << result[1] << " 2 - " << result[2] << " 3 - " << result[3] << endl;
 
