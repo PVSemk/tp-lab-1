dd75ad0e6b152dedca367485f18b1104a4c40063
Chernozemova <44014905+Chernozemova@users.noreply.github.com>
2018-10-23 14:39:53 +0300

    Add files via upload

---
+++ include/task3.h
@@ -0,0 +1,9 @@
+#ifndef TASK3_H
+#define TASK3_H
+
+#include <task2.h>
+
+unsigned long long sumPrime(unsigned int hbound);
+
+
+#endif
---
+++ include/task4.h
@@ -0,0 +1,8 @@
+#ifndef TASK4_H
+#define TASK4_H
+#include <string.h>
+#include <cstring>
+
+char * sum(char *x, char *y);
+
+#endif
