dc2acd871641fcfcb63a841fe891c0ef6bd2fe68
dovahorse98 <dlots42@gmail.com>
2018-10-24 09:25:40 +0300

    check lab1

--- src/task1.cpp
+++ src/task1.cpp
@@ -1,22 +1,18 @@
 #include <task1.h>
 
-long long Nod(long long a, long long b)
-{
-	long long t;
-   if (a<b) { t =a; a=b; b=t; }
-   while(b!=0) {
-     t = b;
-     b = a%b;
-     a = t;
-  }
-   return a;
-}
-
 unsigned long findValue(unsigned int min, unsigned max)
 {
 	long long value = 1;
+	long long t, a, b;
 	for (int i = min; i <= max; i++) {
-		value = value*i / Nod(value, i);
+		a = value; b = i;
+		if (a<b) {t=a;a=b;b=t;}
+		while(b!=0) {
+			t=b;
+			b= a%b;
+			a = t;
+		}
+		value = value*i / a;
 	}
 	return value;
 }
