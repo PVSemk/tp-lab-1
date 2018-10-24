300049c10857c1178fadc7c1644fcf6d47d55fae
dovahorse98 <dlots42@gmail.com>
2018-10-24 09:38:31 +0300

    check lab1

--- src/task3.cpp
+++ src/task3.cpp
@@ -1,6 +1,6 @@
 #include <task3.h>
 
-unsigned long sumPrime(unsigned int hbound) {
+unsigned long long sumPrime(unsigned int hbound) {
 		unsigned long long sum = 0;
 	for (unsigned long i = 2; i < hbound; i++) {
 		if (checkPrime(i)) sum = sum+i;
