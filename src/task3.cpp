1a83271f712bf329cebc601be2076093de3cd50e
Pavel Semkin <pvsemk@mail.ru>
2019-03-10 23:54:52 +0300

    Task3 corrected

--- src/task3.cpp
+++ src/task3.cpp
@@ -7,12 +7,12 @@ unsigned long long sumPrime(unsigned int hbound)
     std::vector<bool> list(hbound, true);
     list[0] = list[1] = false;
     unsigned long long sum = 0;
-    for (unsigned i(2); i < hbound; i++)
+    for (unsigned long long i(2); i <= hbound; i++)
     {
         if (list[i])
         {
             sum += i;
-            for (unsigned k = i*i; k < hbound; k += i)
+            for (unsigned long long k = i*i; k <= hbound; k += i)
                 list[k] = false;
         }
     }
